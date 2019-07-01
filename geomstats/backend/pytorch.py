d9fa9bf8a85b1b4a936418d42249f95fae9574da
Nina Miolane <ninamio78@gmail.com>
2019-07-01 13:52:00 -0700

    Solve maximum bug pytorch

--- geomstats/backend/pytorch.py
+++ geomstats/backend/pytorch.py
@@ -218,7 +218,7 @@ def dot(a, b):
 
 
 def maximum(a, b):
-    return int(torch.max(array(a), array(b)))
+    return torch.max(array(a), array(b))
 
 
 def greater(a, b):
@@ -342,6 +342,7 @@ def where(*args, **kwargs):
 
 def tile(x, y):
     # TODO(johmathe): Native tile implementation
+    y = tuple([int(one_y) for one_y in y])
     return array(np.tile(x, y))
 
 
